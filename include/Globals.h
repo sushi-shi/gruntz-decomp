2df2d4b9605c498bcc2dfc96ac1cad46fa2467a8
sushi-shi <sheepianka@proton.me>
2026-08-29 17:21:32 +0000

    match: restore remaining global helper family

--- include/Globals.h
+++ include/Globals.h
@@ -1,6 +1,53 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
 
+#include <Win32.h>
+
+#include <math.h>
+
+inline BOOL PtInRect(RECT* pRect, int x, int y) {
+    if (x >= pRect->right || x < pRect->left || y >= pRect->bottom || y < pRect->top) {
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+template<class TYPE> class CRange {
+public:
+    CRange() {}
+
+    CRange(TYPE fMin, TYPE fMax) {
+        m_fMin = fMin;
+        m_fMax = fMax;
+    }
+
+    void Set(TYPE fMin, TYPE fMax) {
+        m_fMin = fMin;
+        m_fMax = fMax;
+    }
+
+    TYPE GetMin() {
+        return m_fMin;
+    }
+
+    TYPE GetMax() {
+        return m_fMax;
+    }
+
+protected:
+    TYPE m_fMin;
+    TYPE m_fMax;
+};
+
+template<class T> inline T MinAbs(T a, T b) {
+    return static_cast<T>(fabs(a)) < static_cast<T>(fabs(b)) ? a : b;
+}
+
+template<class T> inline T MaxAbs(T a, T b) {
+    return static_cast<T>(fabs(a)) > static_cast<T>(fabs(b)) ? a : b;
+}
+
 template<class T> inline T Min(T a, T b) {
     return a < b ? a : b;
 }
